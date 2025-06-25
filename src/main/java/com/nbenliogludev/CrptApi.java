a327cf7053fc0d49d1b8e325c3a402501216305d
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-06-25 21:29:52 +0300

    feat: add constructor CrptApi.

--- src/main/java/com/nbenliogludev/CrptApi.java
+++ src/main/java/com/nbenliogludev/CrptApi.java
@@ -1,7 +1,24 @@
 package com.nbenliogludev;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author nbenliogludev
  */
 public class CrptApi {
+
+    private final int requestLimit;
+    private final long windowMillis;
+    private final Deque<Long> requestTimestamps = new ArrayDeque<>();
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        if (requestLimit <= 0) {
+            throw new IllegalArgumentException("requestLimit must be positive");
+        }
+
+        this.requestLimit = requestLimit;
+        this.windowMillis = timeUnit.toMillis(1);
+    }
 }
