1b662ace608e5fdde11cd67471bf472caf9e48b6
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-06-26 01:57:01 +0300

    feat: add DocumentType to CrptApi.

--- src/main/java/com/nbenliogludev/CrptApi.java
+++ src/main/java/com/nbenliogludev/CrptApi.java
@@ -1,5 +1,6 @@
 package com.nbenliogludev;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -88,6 +89,70 @@ public class CrptApi {
             this.signature = signatureB64;
             this.type = type;
         }
+    }
+
+    public enum DocumentType {
+        AGGREGATION_DOCUMENT,
+        AGGREGATION_DOCUMENT_CSV,
+        AGGREGATION_DOCUMENT_XML,
+        DISAGGREGATION_DOCUMENT,
+        DISAGGREGATION_DOCUMENT_CSV,
+        DISAGGREGATION_DOCUMENT_XML,
+        REAGGREGATION_DOCUMENT,
+        REAGGREGATION_DOCUMENT_CSV,
+        REAGGREGATION_DOCUMENT_XML,
+        LP_INTRODUCE_GOODS,
+        LP_SHIP_GOODS,
+        LP_SHIP_GOODS_CSV,
+        LP_SHIP_GOODS_XML,
+        LP_INTRODUCE_GOODS_CSV,
+        LP_INTRODUCE_GOODS_XML,
+        LP_ACCEPT_GOODS,
+        LP_ACCEPT_GOODS_XML,
+        LK_REMARK,
+        LK_REMARK_CSV,
+        LK_REMARK_XML,
+        LK_RECEIPT,
+        LK_RECEIPT_XML,
+        LK_RECEIPT_CSV,
+        LP_GOODS_IMPORT,
+        LP_GOODS_IMPORT_CSV,
+        LP_GOODS_IMPORT_XML,
+        LP_CANCEL_SHIPMENT,
+        LP_CANCEL_SHIPMENT_CSV,
+        LP_CANCEL_SHIPMENT_XML,
+        LK_KM_CANCELLATION,
+        LK_KM_CANCELLATION_CSV,
+        LK_KM_CANCELLATION_XML,
+        LK_APPLIED_KM_CANCELLATION,
+        LK_APPLIED_KM_CANCELLATION_CSV,
+        LK_APPLIED_KM_CANCELLATION_XML,
+        LK_CONTRACT_COMMISSIONING,
+        LK_CONTRACT_COMMISSIONING_CSV,
+        LK_CONTRACT_COMMISSIONING_XML,
+        LK_INDI_COMMISSIONING,
+        LK_INDI_COMMISSIONING_CSV,
+        LK_INDI_COMMISSIONING_XML,
+        LP_SHIP_RECEIPT,
+        LP_SHIP_RECEIPT_CSV,
+        LP_SHIP_RECEIPT_XML,
+        OST_DESCRIPTION,
+        OST_DESCRIPTION_CSV,
+        OST_DESCRIPTION_XML,
+        CROSSBORDER,
+        CROSSBORDER_CSV,
+        CROSSBORDER_XML,
+        LP_INTRODUCE_OST,
+        LP_INTRODUCE_OST_CSV,
+        LP_INTRODUCE_OST_XML,
+        LP_RETURN,
+        LP_RETURN_CSV,
+        LP_RETURN_XML,
+        LP_SHIP_GOODS_CROSSBORDER,
+        LP_SHIP_GOODS_CROSSBORDER_CSV,
+        LP_SHIP_GOODS_CROSSBORDER_XML,
+        LP_CANCEL_SHIPMENT_CROSSBORDER,
+        @JsonEnumDefaultValue UNKNOWN
     }
 
     public enum DocumentFormat { MANUAL, XML, CSV }
